10b0a0c18d7f7f20dcbbe57d7fd53368c088ae2d
dima9997038 <dima9997038@gmail.com>
2021-03-08 20:40:06 +0300

    listMusic

---
+++ src/main/java/by/dima/simple/CountryMusic.java
@@ -0,0 +1,8 @@
+package by.dima.simple;
+
+public class CountryMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Sallie Gooden";
+    }
+}
--- src/main/java/by/dima/simple/MusicPlayer.java
+++ src/main/java/by/dima/simple/MusicPlayer.java
@@ -1,22 +1,35 @@
 package by.dima.simple;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
-    private Music music;
+
+
+    private List<Music> musicList= new ArrayList<>();
     private String name;
     private int volume;
 
-    public void setMusic(Music music) {
-        this.music = music;
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
     }
+//    public void setMusic(Music music) {
+//        this.music = music;
+//    }
 
     public MusicPlayer() {
     }
 
-    public MusicPlayer(Music music) {
-        this.music=music;
-    }
-    public void playMusic(){
-        System.out.println("Playing: "+ music.getSong());
+//    public MusicPlayer(Music music) {
+//        this.musicList=musicList;
+//    }
+
+     void playMusic(){
+
+        //System.out.println("Playing: "+ music.getSong());
+        for (Music m:musicList ) {
+            System.out.println("Playing: " + m.getSong());
+        }
     }
 
     public String getName() {
