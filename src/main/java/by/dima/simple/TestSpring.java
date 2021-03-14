99366458cfd44a7c7d8400e5141d0d27c456d289
dima9997038 <dima9997038@gmail.com>
2021-03-14 10:57:05 +0300

    Annotation

--- src/main/java/by/dima/simple/TestSpring.java
+++ src/main/java/by/dima/simple/TestSpring.java
@@ -6,13 +6,16 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context= new ClassPathXmlApplicationContext("applicationContext.xml");
 
-       // Music music =context.getBean("musicBean", Music.class);
-       // MusicPlayer musicPlayer= new MusicPlayer(music);
-        MusicPlayer musicPlayer=context.getBean("musicPlayer", MusicPlayer.class);
+        Music music =context.getBean("someRocklMusic", Music.class);
+        Music music2 =context.getBean("someClassicalMusic", Music.class);
+        MusicPlayer musicPlayer= new MusicPlayer(music);
+        MusicPlayer musicPlayer2= new MusicPlayer(music2);
+//        MusicPlayer musicPlayer=context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusic();
+        musicPlayer2.playMusic();
 
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
+//        System.out.println(musicPlayer.getName());
+//        System.out.println(musicPlayer.getVolume());
         context.close();
 
     }
